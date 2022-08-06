8dab35514968be3b1869c36daa069d994d891e54
Paloma <casagrande.paloma@gmail.com>
2022-08-06 11:24:39 -0300

    [feat] (delete /urls/:id) deletes the url from the db by the correct authentication from user

--- src/controllers/urlsController.js
+++ src/controllers/urlsController.js
@@ -60,10 +60,34 @@ export async function openShortUrl(req, res){
 
         const newVisitCount = parseInt(url[0].visitCount) + 1;
         await connection.query(`UPDATE urls SET "visitCount" = $1 WHERE "shortUrl" = $2;`, [newVisitCount, shortUrl]);
-        console.log(url, newVisitCount, shortUrl, url[0].url);
         res.redirect(url[0].url);
     } catch (error) {
         console.log(error);
         res.sendStatus(500);
     }
-}
+};
+
+export async function deleteUrl(req, res){
+    const {user} = res.locals;
+    const userId = user[0].id;
+    console.log(userId);
+    const {id} = req.params;
+    if(isNaN(parseInt(id))) return res.sendStatus(400)
+
+    try {
+ 
+        const {rows: url, rowCount } = await connection.query(`SELECT * FROM urls WHERE id = $1;`, [id]);
+        if(rowCount === 0) return res.sendStatus(404)
+        const urlUserId = url[0].userId;
+
+        console.log(userId, urlUserId, id)
+
+        if(userId != urlUserId) return res.sendStatus(401)
+
+        await connection.query(` DELETE FROM urls WHERE urls.id = $1;`,[id]);
+        res.sendStatus(204);
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+};
--- src/routes/urlsRouter.js
+++ src/routes/urlsRouter.js
@@ -1,11 +1,11 @@
 import { Router } from 'express';
-import { getUrl, openShortUrl, shortenUrl } from '../controllers/urlsController.js';
+import { deleteUrl, getUrl, openShortUrl, shortenUrl } from '../controllers/urlsController.js';
 import { userMiddleware } from '../middlewares/userMiddleware.js';
 
 const urlsRouter = Router();
 urlsRouter.post('/urls/shorten',userMiddleware, shortenUrl);
 urlsRouter.get('/urls/:id', getUrl);
 urlsRouter.get('/urls/open/:shortUrl', openShortUrl);
-urlsRouter.delete('/urls/:id', ()=> console.log('delete urls:id'));
+urlsRouter.delete('/urls/:id', userMiddleware, deleteUrl);
 
 export default urlsRouter;
