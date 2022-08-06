bf673bf8453df5028dc235332925ff34077f88b0
Paloma <casagrande.paloma@gmail.com>
2022-08-06 10:12:34 -0300

    [feat] (get /urls/:id) return id, shorturl, url of db of a certain id

--- src/controllers/urlsController.js
+++ src/controllers/urlsController.js
@@ -29,4 +29,22 @@ export async function shortenUrl(req, res){
         console.log(error);
         res.sendStatus(500);
     }
+};
+
+export async function getUrl(req, res){
+    const {id} = req.params;
+
+    if(isNaN(parseInt(id))) return res.sendStatus(400)
+
+    try {
+ 
+        const {rows: url, rowCount } = await connection.query(`SELECT id, "shortUrl", url FROM urls WHERE id = $1;`, [id]);
+
+        if(rowCount === 0) return res.sendStatus(404)
+
+        res.status(200).send(url[0]);
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
 }
